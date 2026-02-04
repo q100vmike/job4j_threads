d5512822e676b457f2b259804c1c6e4dad0f41bf
q100vmike <melkin@mail.ru>
2026-02-04 17:06:21 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable{
+
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        int count = 0;
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    Thread.sleep(500);
+                    System.out.print("\r load: " + process[count]);
+                    if (++count == 4) {count = 0;}
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000); /* симулируем выполнение параллельной задачи в течение 5 секунд. */
+        progress.interrupt();
+    }
+}
