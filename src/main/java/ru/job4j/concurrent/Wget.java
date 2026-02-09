dbc7b038a79291407db764c5caac4a33de6ea5bb
q100vmike <melkin@mail.ru>
2026-02-09 17:07:18 +0300

    8. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,20 +1,47 @@
 package ru.job4j.concurrent;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.util.regex.Pattern;
+import  java.net.URL;
 
-public class Wget {
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
 
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        for (int index = 0; index <= 100; index++) {
-                            System.out.print("\rLoading : " + index  + "%");
-                            Thread.sleep(1000);
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-        );
-        thread.start();
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        /* Скачать файл*/
+    }
+
+    private static void checkArguments(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("No Arguments found!");
+        }
+
+        Pattern pattern = Pattern.compile("^\\d+");
+        if (!pattern.matcher(args[1]).matches()) {
+            throw new IllegalArgumentException("Invalid 2'nd Argument!");
+        }
+
+        try {
+            new URL(args[0]).toURI();
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new IllegalArgumentException("Invalid 1'st Argument!");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        checkArguments(args);
+
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
