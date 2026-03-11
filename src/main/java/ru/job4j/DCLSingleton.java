7f9f5b302a02cbf99460db9153c119cbb14943d2
q100vmike <melkin@mail.ru>
2026-03-11 17:21:53 +0300

    2. Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/DCLSingleton.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public final class DCLSingleton {
+
+    private static volatile DCLSingleton instance;
+
+    public static DCLSingleton getInstance() {
+                if (instance == null) {
+                    instance = new DCLSingleton();
+                }
+        return instance;
+    }
+
+    private DCLSingleton() {
+    }
+
+}
