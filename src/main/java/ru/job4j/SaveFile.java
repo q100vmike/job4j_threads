f2868ca19a5e9fd69301b26ed8d2e19f9bf0046b
q100vmike <melkin@mail.ru>
2026-03-24 10:59:18 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/SaveFile.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SaveFile {
+    private File file;
+
+    public SaveFile(File file) {
+        this.file = file;
+    }
+
+    public void saveContent(String content) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(content);
+        }
+    }
+}
