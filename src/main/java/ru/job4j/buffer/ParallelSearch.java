487627cf2fbdf3c88c0e15ed8e3aa068f42da8a0
q100vmike <melkin@mail.ru>
2026-04-21 10:39:29 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,41 @@
+package ru.job4j.buffer;
+
+
+import ru.job4j.queue.SimpleBlockingQueue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(10);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+        ).start();
+    }
+}
