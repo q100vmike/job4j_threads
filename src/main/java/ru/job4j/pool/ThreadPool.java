4518438c83897c3dd8cb03bc3ddf72b68449d76e
q100vmike <melkin@mail.ru>
2026-05-07 17:26:02 +0300

    1. Реализовать ThreadPool [#1099]

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -1,10 +1,11 @@
 package ru.job4j.pool;
 
-import org.testng.util.TimeUtils;
 import ru.job4j.queue.SimpleBlockingQueue;
 
 import java.util.LinkedList;
 import java.util.List;
+
+import static java.lang.Thread.sleep;
 
 public class ThreadPool {
     private final List<Thread> threads = new LinkedList<>();
@@ -13,21 +14,47 @@ public class ThreadPool {
     public ThreadPool() {
         int size = Runtime.getRuntime().availableProcessors();
         for (int i = 0; i < size; i++) {
-            threads.add(new Thread(() -> {
-                try {
-                    tasks.poll();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+            Thread th = new Thread(() -> {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        var r = tasks.poll();
+                        if (r != null) {
+                            r.run();
+                        }
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
-            }));
+            });
+            th.start();
+            threads.add(th);
         }
     }
 
-    public void work(Runnable job) {
-
+    public void work(Runnable job) throws InterruptedException {
+        tasks.offer(job);
     }
 
-    public void shutdown() {
+    public void shutdown() throws InterruptedException {
+        for (int i = 0; i < threads.size(); i++) {
+            threads.get(i).interrupt();
+        }
+        for (int i = 0; i < threads.size(); i++) {
+            threads.get(i).join();
+        }
+    }
 
+    public static void main(String[] args) throws InterruptedException {
+        Runnable job = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("run" + Thread.currentThread().getName());
+            }
+        };
+        ThreadPool pool = new ThreadPool();
+        for (int i = 0; i < 10; i++) {
+            pool.work(job);
+        }
+        pool.shutdown();
     }
 }
