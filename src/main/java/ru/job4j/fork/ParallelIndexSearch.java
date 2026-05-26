3127218018fce388f350554125b8fd69124dd5ee
q100vmike <melkin@mail.ru>
2026-05-26 17:11:43 +0300

    3. ForkJoinPool [#315067]

--- src/main/java/ru/job4j/fork/ParallelIndexSearch.java
+++ src/main/java/ru/job4j/fork/ParallelIndexSearch.java
@@ -8,17 +8,21 @@ public class ParallelIndexSearch<T> extends RecursiveTask<Integer> {
 
     private final List<?> array;
     private final T obj;
-
+    private final int from;
+    private final int to;
     //public T getObj() { return obj; }
 
-    public ParallelIndexSearch(List<?> array, T obj) {
+    public ParallelIndexSearch(List<?> array, T obj, int from, int to) {
         this.array = array;
         this.obj = obj;
+        this.from = from;
+        this.to = to;
     }
 
     @Override
     protected Integer compute() {
         int len = array.size();
+        int j = -1;
 
         if (len <= 10) {
             for (int i = 0; i < len; i++) {
@@ -28,6 +32,16 @@ public class ParallelIndexSearch<T> extends RecursiveTask<Integer> {
             }
         }
 
-        return 0;
+        int middle = (from + to) / 2;
+        // создаем задачи для сортировки частей
+        ParallelIndexSearch leftSort = new ParallelIndexSearch(array, obj, from, middle);
+        ParallelIndexSearch rightSort = new ParallelIndexSearch(array, obj, middle + 1, to);
+        // производим деление.
+        // оно будет происходить, пока в частях не останется по одному элементу
+        leftSort.fork();
+        rightSort.fork();
+
+
+        return j;
     }
 }
